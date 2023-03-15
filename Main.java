f2f23632d9634d542adeb25f9a00297f8bc2f4b3
JakubSx01 <126695976+JakubSx01@users.noreply.github.com>
2023-03-15 15:35:41 +0000

    suma2

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
     int suma=0;
     System.out.println("suma kolejnch liczb parzystych");
     for(int i=0;i<=100;i+=2){
-      System.out.println(suma+=i);};
-    
+      suma+=i;};
+    System.out.println(suma);
   }
 }
